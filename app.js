36ad7cc0ef8c67d2151d935d01c47daf57c8a1c7
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-09-05 22:55:04 +0200

    set controls on req for id value

--- app.js
+++ app.js
@@ -13,22 +13,48 @@ app.get("/posts", (req, res) => {
 
 //show
 app.get("/posts/:id", (req, res) => {
-    res.send("Post con id" + req.params.id);
+    if (isNaN(Number(req.params.id))) {
+        res.status(404).json({
+            "message": "bad request: id must be a number"
+        })
+        return
+    }
+
+    res.send("Post con id " + req.params.id);
 })
 
 //update
 app.put("/posts/:id", (req, res) => {
-    res.send("modifica del post con id" + req.params.id);
+    if (isNaN(Number(req.params.id))) {
+        res.status(404).json({
+            "message": "bad request: id must be a number"
+        })
+        return
+    }
+
+    res.send("modifica del post con id " + req.params.id);
 })
 
 //modify
 app.patch("/posts/:id", (req, res) => {
-    res.send("modifica parziale del post con id" + req.params.id);
+    if (isNaN(Number(req.params.id))) {
+        res.status(404).json({
+            "message": "bad request: id must be a number"
+        })
+        return
+    }
+    res.send("modifica parziale del post con id " + req.params.id);
 })
 
 //destroy
 app.delete("/posts/:id", (req, res) => {
-    res.send("cancellazione parziale del post con id" + req.params.id);
+    if (isNaN(Number(req.params.id))) {
+        res.status(404).json({
+            "message": "bad request: id must be a number"
+        })
+        return
+    }
+    res.send("cancellazione del post con id " + req.params.id);
 })
 
 app.listen(port, () => {
