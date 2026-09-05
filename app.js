d3564165ce6d9843f7d6ed52a18e3e1958628be6
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-09-05 22:44:19 +0200

    initial commit: assets + create paths without express routing

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import posts from './database.js';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+//index
+app.get("/posts", (req, res) => {
+    res.send("Lista dei post");
+})
+
+//show
+app.get("/posts/:id", (req, res) => {
+    res.send("Post con id" + req.params.id);
+})
+
+//update
+app.put("/posts/:id", (req, res) => {
+    res.send("modifica del post con id" + req.params.id);
+})
+
+//modify
+app.patch("/posts/:id", (req, res) => {
+    res.send("modifica parziale del post con id" + req.params.id);
+})
+
+//destroy
+app.delete("/posts/:id", (req, res) => {
+    res.send("cancellazione parziale del post con id" + req.params.id);
+})
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`
+    );
+})
---
+++ database.js
@@ -0,0 +1,60 @@
+const posts = [
+    {
+        id: 1,
+        title: "ciambellone",
+        img_src: "images/ciambellone.jpeg",
+        content: "per fare il ciambellone...",
+        tags: [
+            "dolce",
+            "ciambellone",
+            "buono"
+        ]
+    },
+    {
+        id: 2,
+        title: "cracker alla barbabietola",
+        img_src: "images/cracker_barbabietola.jpeg",
+        content: "per fare i cracker alla barbabietola...",
+        tags: [
+            "cracker",
+            "salato",
+            "barbabietola"
+        ]
+    },
+    {
+        id: 3,
+        title: "pane fritto dolce",
+        img_src: "images/pane_fritto_dolce.jpeg",
+        content: "per fare il pane fritto dolce...",
+        tags: [
+            "dolce",
+            "pane",
+            "buono"
+        ]
+    },
+    {
+        id: 4,
+        title: "pasta alla barbabietola",
+        img_src: "images/pasta_barbabietola.jpeg",
+        content: "per fare la pasta alla barbabietola...",
+        tags: [
+            "salato",
+            "pasta",
+            "barbabietola"
+        ]
+    },
+    {
+        id: 5,
+        title: "torta paesana",
+        img_src: "images/torta_paesana.jpeg",
+        content: "per fare il la torta paesana...",
+        tags: [
+            "dolce",
+            "torta",
+            "paesana"
+        ]
+    },
+
+];
+
+export default posts;
